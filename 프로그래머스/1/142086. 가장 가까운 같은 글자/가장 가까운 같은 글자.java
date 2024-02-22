343de00aa55b0f6c218d3459f6df998852f57501
yubin113 <54315516+yubin113@users.noreply.github.com>
2024-02-22 21:48:02 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 25.02 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            if(i == 0){
+                answer[i] = -1;
+            }else{
+                int idx = s.substring(0, i).lastIndexOf(s.charAt(i));
+                if(idx == -1){
+                    answer[i] = idx;
+                }else{
+                    answer[i] = i - idx;
+                }
+            }
+        }
+        return answer;
+    }
+}
