84a05c8b7f8aff6db7864dfe30bd886df89258cb
yubin113 <54315516+yubin113@users.noreply.github.com>
2024-03-14 16:41:00 +0900

    [level 1] Title: 소수 찾기, Time: 189.71 ms, Memory: 52 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 2; i <= n; i++) {
+            if(isPrime(i)) answer++;
+        }
+        return answer;
+    }
+    public static boolean isPrime(int num){
+        if(num < 2 ) return false;
+        else if(num == 2) return true;
+
+        for(int i = 2; i * i <= num; i++){
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+}
