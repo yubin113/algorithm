b85e181e1b2845dca691384b1e2bd171898d3787
yubin113 <54315516+yubin113@users.noreply.github.com>
2024-07-10 22:58:09 +0900

    [Bronze V] Title: A×B, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = new int[2];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 2; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int mul = arr[0] * arr[1];
+        bw.write(String.valueOf(mul));
+
+        bw.flush();
+        bw.close();
+
+    }
+}
