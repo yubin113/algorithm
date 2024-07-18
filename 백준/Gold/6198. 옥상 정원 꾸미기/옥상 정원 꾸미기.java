832bcee26b5bcd156e8d42d8f6c6b796a8e46099
yubin113 <54315516+yubin113@users.noreply.github.com>
2024-07-18 16:31:15 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 2648 ms, Memory: 22292 KB -BaekjoonHub

---
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());   //빌딩 번호
+		
+		int[] arr = new int[N];
+		int[] cnt = new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		long result = 0;
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				if(arr[i] > arr[j]) {
+					cnt[i]++;
+				}else {
+					break;
+				}
+			}
+		}
+		
+		for (int i = 0; i < cnt.length; i++) {
+			result += cnt[i];
+		}
+		
+		System.out.println(result);
+	}
+}
+
