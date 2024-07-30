55a81bb59772baacfeca3962bd083c815680ccb2
yubin113 <54315516+yubin113@users.noreply.github.com>
2024-07-30 14:45:46 +0900

    [Gold IV] Title: 문자열 폭발, Time: 608 ms, Memory: 62772 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		String bomb = br.readLine();
+				
+		Stack<Character> stack = new Stack<>();
+		
+		for (int i = 0; i < str.length(); i++) {
+			stack.push(str.charAt(i));
+			
+			if(stack.size() >= bomb.length()) {
+				for (int j = 0; j < bomb.length(); j++) {
+					if(stack.get(stack.size() - bomb.length() + j) != bomb.charAt(j)) {
+						break;
+					}
+					if(j == bomb.length() - 1) {
+						for (int j2 = 0; j2 < bomb.length(); j2++) {
+							stack.pop();
+						}
+					}
+				}
+			}
+		}
+		
+		if(stack.isEmpty()) {
+			System.out.println("FRULA");
+			return;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < stack.size(); i++) {
+			sb.append(stack.get(i));
+		}
+		
+		System.out.println(sb);
+
+	}
+}
