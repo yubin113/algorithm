38708a19b6623f1850d1e787af0f44dd93db12e9
yubin113 <54315516+yubin113@users.noreply.github.com>
2024-07-31 09:28:37 +0900

    [Silver III] Title: N과 M (3), Time: 464 ms, Memory: 68420 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static int N;
+	public static int M;
+	public static int[] arr;
+	public static boolean[] visited;
+	public static StringBuilder sb;
+	
+	public static void main(String[] args) throws Exception{
+        
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[N];
+		visited = new boolean[N];
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = i+1;
+		}
+		
+		sb = new StringBuilder();
+		
+		backTracking(0);
+		
+		System.out.println(sb);
+	}
+	
+	public static void backTracking(int depth) {
+		if(depth == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(arr[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = 0; i < N; i++) {
+			visited[i] = true;
+			arr[depth] = i + 1;
+			backTracking(depth+1);
+			visited[i] = false;
+
+		}
+	}
+}
