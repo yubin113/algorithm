551582557567132eb5a24da3b5aeb673df9a210c
yubin113 <54315516+yubin113@users.noreply.github.com>
2024-08-16 12:10:00 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 165 ms, Memory: 31,696 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -1,85 +1,87 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// double b = 1.0;               
-// char g = 'b';
-// String var = "ABCDEFG";
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-import java.util.Scanner;
+
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
- */
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		/*
-		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
-		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
-		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
-		 */
-		//System.setIn(new FileInputStream("res/input.txt"));
-
-		/*
-		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
-		 */
-		Scanner sc = new Scanner(System.in);
-		/*
-		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-		*/
-
-		for(int test_case = 1; test_case <= 10; test_case++) {
-			sc.nextInt();
-			ArrayList<Integer> arrayList = new ArrayList<>();
-			for (int i = 0; i < 8; i++) {
-				arrayList.add(sc.nextInt());
-			}
-			int i = 1;
-			while(true) {
-				if(i == 6) {
-					i = 1;
-				}
-				int num = arrayList.remove(0);
-				if(num - i <= 0) {
-					arrayList.add(0);
-					break;
-				}else {
-					arrayList.add(num - i);
-				}
-				i++;
-			}
-			System.out.print("#" + test_case + " ");
-			for (int j = 0; j < arrayList.size() - 1; j++) {
-				System.out.print(arrayList.get(j) + " ");
-			}
-			System.out.println(arrayList.get(arrayList.size() - 1));
+public class Solution {
+	/**
+	 * 
+	 * 1. 테스트케이스(10)마다
+	 * 		1.1 여덟 개의 데이터 받기
+	 * 2. 데이터마다 사이클 돌기
+	 * 		2.1 마지막 수가 0보다 작거나 같을때까지 while문 돌기
+	 * 		2.2 사이클(5)마다,
+	 * 			2.2.1 자리 옮기기
+	 */
+	static BufferedReader br;
+	static StringTokenizer st;
+	static int tc;
+	static StringBuilder sb;
+	static int[] beforePassword;
+	static int[] afterPassword;
+	public static void main(String[] args) throws Exception{
+		
+		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		
+		//1. 테스트케이스(10)마다
+		for (int i = 1; i <= 10; i++) {
 			
+			inputCase();
+			
+			sb.append("#").append(i).append(" ");
+			//2. 데이터마다 사이클 돌기
+			cycle(beforePassword);
+			sb.append("\n");
+			
+		}
+		
+		System.out.println(sb);
+	}
+	
+	public static void inputCase() throws Exception{
+		
+		tc = Integer.parseInt(br.readLine().trim());
+		
+		//1.1 여덟 개의 데이터 받기
+		beforePassword = new int[8];
+		st = new StringTokenizer(br.readLine().trim());
+		for (int i = 0; i < 8; i++) {
+			beforePassword[i] = Integer.parseInt(st.nextToken());
+		}
+		
+	}
+	
+	public static void cycle(int[] nums) {
+		ArrayList<Integer> arrayList = new ArrayList<>();
+		for (int i = 0; i < 8; i++) {
+			arrayList.add(nums[i]);
+		}
+		
+		//2.1 마지막 수가 0보다 작거나 같을때까지 while문 돌기
+		loopa:
+		while(arrayList.get(arrayList.size() - 1) > 0) {
+			//2.2 사이클(5)마다,
+			for (int i = 0; i < 5; i++) {
+				//2.2.1 자리 옮기기
+				int first = arrayList.remove(0);
+				first = first - (i + 1);
+				if(first <= 0) {
+					arrayList.add(0);
+					break loopa;
+				}
+				arrayList.add(first);
+				
+			}
+		}
+		
+		
+		for (int i = 0; i < arrayList.size(); i++) {
+			sb.append(arrayList.get(i)).append(" ");
 		}
 	}
 }
