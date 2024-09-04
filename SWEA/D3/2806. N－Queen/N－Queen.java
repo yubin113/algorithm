dfb56efd0cb5f8391be9b7f5d016423e17473ab6
yubin113 <54315516+yubin113@users.noreply.github.com>
2024-09-04 09:16:16 +0900

    [D3] Title: N-Queen, Time: 164 ms, Memory: 33,504 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.java
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -1,76 +1,51 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// double b = 1.0;               
-// char g = 'b';
-// String var = "ABCDEFG";
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-import java.util.Scanner;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
- */
-class Solution
-{
-	static int answer = 0;
-	static int[] chess;
-	static int N;
+public class Solution {
+	static BufferedReader br;
+	static StringTokenizer st;
+	static StringBuilder sb;
 	
-	public static void main(String[] args) throws Exception {
+	static int TC;
+	static int N;
+	static int[] col;
+	
+	static int answer;
+	public static void main(String[] args) throws IOException{
+		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
 		
-		Scanner sc= new Scanner(System.in);
-		int T = sc.nextInt();
-		
-		for(int test_case = 1; test_case <= T; test_case++) {
+		TC = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= TC; tc++) {
+			N = Integer.parseInt(br.readLine());
+			
 			answer = 0;
+			col = new int[N + 1];
+			setQueen(1);
 			
-			N = sc.nextInt();
-			chess = new int[N];
-			
-			dfs(0);
-			
-			System.out.println("#" + test_case + " " + answer);
+			sb.append("#").append(tc).append(" ").append(answer).append("\n");
 		}
-		
-		
+		System.out.println(sb);
 	}
-	static void dfs(int depth) {
-		if(depth == N) {
+	static void setQueen(int row) {
+		if(!check(row-1)) return;
+		
+		if(row > N) {
 			answer++;
 			return;
 		}
-		for(int i = 0; i < N; i++) {
-			chess[depth] = i;
-			if(isPossible(depth)) {
-				dfs(depth + 1);
-			}
+		
+		for (int i = 1; i <= N; i++) {
+			col[row] = i;
+			setQueen(row + 1);
 		}
 	}
-	static boolean isPossible(int depth) {
-		for(int i = 0; i < depth; i++) {
-			if(chess[i] == chess[depth] || depth - i == Math.abs(chess[depth] - chess[i])) {
+	static boolean check(int row) {
+		for (int i = 1; i < row; i++) {
+			if(col[row] == col[i] || row-i == Math.abs(col[row]-col[i])) {
 				return false;
 			}
 		}
