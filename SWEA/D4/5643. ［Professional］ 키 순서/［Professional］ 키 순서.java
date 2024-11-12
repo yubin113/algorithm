47f6e31fd38b651c3ecf68bcea2a35a6ae01f138
yubin113 <54315516+yubin113@users.noreply.github.com>
2024-11-12 10:23:06 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,783 ms, Memory: 92,168 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -1,63 +1,69 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution {
-    static BufferedReader br;
-    static StringTokenizer st;
-    static StringBuilder sb;
-
-    public static void main(String[] args) throws IOException {
-        
-        sb = new StringBuilder();
-
-        br = new BufferedReader(new InputStreamReader(System.in));
-
-        int TC = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= TC; tc++) {
-            int N, M, answer = 0;
-            int a,b;
-            boolean[][] distance;
-
-            N = Integer.parseInt(br.readLine());
-            M = Integer.parseInt(br.readLine());
-
-            distance = new boolean[N+1][N+1];
-            for (int i = 0; i < M; i++) {
-                st = new StringTokenizer(br.readLine());
-
-                a = Integer.parseInt(st.nextToken());
-                b = Integer.parseInt(st.nextToken());
-
-                distance[a][b] = true;
-            }
-
-            for (int k = 1; k <= N; k++) {
-                for (int i = 1; i <= N; i++) {
-                    for (int j = 1; j <= N; j++) {
-                        if(distance[i][k] && distance[k][j]){
-                            distance[i][j] = true;
-                        }
-                    }
-                }
-            }
-
-            boolean check;
-            for (int i = 1; i <= N ; i++) {
-                check = false;
-                for (int j = 1; j <= N ; j++) {
-                    if(i != j && !distance[j][i] && !distance[i][j]){
-                        check = true;
-                        break;
-                    }
-                }
-                if(!check) answer++;
-            }
-
-            sb.append("#").append(tc).append(" ").append(answer).append("\n");
-        }
-        System.out.println(sb);
-    }
+public class Solution{
+	/**
+	 * SWEA 5643 D4 키 순서
+	 * 풀이 방법: 플로이드 워셜
+	 */
+	
+	static BufferedReader br;
+	static StringTokenizer st;
+	static StringBuilder sb;
+	
+	static int TC;
+	static int N,M;
+	static boolean[][] adjList;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		
+		TC = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= TC; tc++) {
+			N = Integer.parseInt(br.readLine()); //학생들의 수
+			M = Integer.parseInt(br.readLine()); //두 학생들의 키를 비교한 횟수
+			
+			adjList = new boolean[N+1][N+1];
+			
+			for (int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int student1 = Integer.parseInt(st.nextToken());
+				int student2 = Integer.parseInt(st.nextToken());
+				
+				adjList[student1][student2] = true;
+			}
+			
+			for (int k = 1; k < N+1; k++) {
+				for (int i = 1; i < N+1; i++) {
+					for (int j = 1; j < N+1; j++) {
+						if(adjList[i][k] && adjList[k][j]) adjList[i][j] = true;
+					}
+				}
+			}
+			
+			int resultStudents = 0;
+			for (int i = 1; i < N+1; i++) {
+				boolean check = true;
+				for (int j = 1; j < N+1; j++) {
+					//자기 자신은 패스
+					if(i == j) continue;
+					if(!adjList[i][j] ) {
+						if(!adjList[j][i]) {
+							check = false;
+							break;
+						}
+					}
+				}
+				if(check) resultStudents++;
+			}
+			
+			sb.append("#").append(tc).append(" ").append(resultStudents).append("\n");
+		}
+		System.out.println(sb);
+	}
 
 }
