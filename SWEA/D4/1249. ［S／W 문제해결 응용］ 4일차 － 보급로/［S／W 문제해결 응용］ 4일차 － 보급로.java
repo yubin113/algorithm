8870f6f0538a7084efb7bb6c7af05c86fd038636
yubin113 <54315516+yubin113@users.noreply.github.com>
2024-11-12 10:02:59 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 142 ms, Memory: 23,984 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -1,64 +1,103 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
 
 public class Solution {
-	static int N, map[][];
-	static int dr[] = {-1,1,0,0};
-	static int dc[] = {0,0,-1,1};
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
-		int TC = Integer.parseInt(br.readLine());
+	/**
+	 * SWEA 1249 D4 보급로
+	 * 풀이: 다익스트라
+	 * 		
+	 */
+	
+	static BufferedReader br;
+	static StringTokenizer st;
+	static StringBuilder sb;
+	
+	static int TC;
+	static int N;
+	static int[][] arr;
+	static int[][] distance;
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
+	
+	static int result;
+	
+	static class Node implements Comparable<Node>{
+		int x,y,time;
+
+		public Node(int x, int y, int time) {
+			super();
+			this.x = x;
+			this.y = y;
+			this.time = time;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return this.time - o.time;
+		}
 		
+	}
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		
+		TC = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= TC; tc++) {
 			N = Integer.parseInt(br.readLine());
-			map = new int[N][N];
+			arr = new int[N][N];
+			distance = new int[N][N];
 			
 			for (int i = 0; i < N; i++) {
-				char[] ch = br.readLine().toCharArray();
+				String tmp = br.readLine();
 				for (int j = 0; j < N; j++) {
-					map[i][j] = ch[j] - '0';
+					arr[i][j] = tmp.charAt(j) - '0';
+					distance[i][j] = Integer.MAX_VALUE;
 				}
 			}
-			System.out.println("#" + tc + " " + getMinTime(0, 0, N - 1, N-1));
+			
+			result = Integer.MAX_VALUE;
+			BFS();
+			
+			sb.append("#").append(tc).append(" ").append(distance[N-1][N-1]).append("\n");
 		}
+		System.out.println(sb);
+	
 	}
-	static int getMinTime(int sr, int sc, int er, int ec) {
-		final int INF = Integer.MAX_VALUE;
-		boolean[][] visited = new boolean[N][N];
-		int[][] minTime = new int[N][N];
-		PriorityQueue<int[]> pQueue = new PriorityQueue<>((a,b) -> Integer.compare(a[2],b[2]));
+	
+	static void BFS() {
+		PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
+		priorityQueue.add(new Node(0,0,0));
+		distance[0][0] = 0;
 		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				minTime[i][j] = INF;
+		while(!priorityQueue.isEmpty()) {
+			Node now = priorityQueue.poll();
+			
+			//이미 현재 시간이 넘은 경우
+			if(now.time > distance[now.x][now.y]) continue;
+			
+			for (int i = 0; i < 4; i++) {
+				int nextX = now.x + dx[i];
+				int nextY = now.y + dy[i];
+				
+				
+				//경계
+				if(nextX < 0 || nextY < 0 || nextX >= N || nextY >= N) continue;
+				
+				//더 작은경우만
+				int nextTime = now.time + arr[nextX][nextY];
+				if(nextTime < distance[nextX][nextY]) {
+					distance[nextX][nextY] = nextTime;
+					priorityQueue.add(new Node(nextX, nextY, nextTime));
+				}
+				
 			}
 		}
 		
-		 minTime[sr][sc] = 0;
-		 pQueue.offer(new int[] {sr,sc, minTime[sr][sc]});
-		 
-		 while(!pQueue.isEmpty()) {
-			 int[] stopOver = pQueue.poll();
-			 int r = stopOver[0];
-			 int c = stopOver[1];
-			 int time = stopOver[2];
-			 
-			 if(visited[r][c]) continue;
-			 visited[r][c] = true;
-			 if(r==er && c==ec) return time;
-			 
-			 for (int d = 0; d < 4; d++) {
-				int nr = r + dr[d];
-				int nc = c + dc[d];
-				if(nr>=0 && nr < N && nc>=0 && nc < N && !visited[nr][nc] && minTime[nr][nc] > time + map[nr][nc]) {
-					minTime[nr][nc] = time + map[nr][nc];
-					pQueue.offer(new int[] {nr,nc,minTime[nr][nc]});
-				}
-			}
-			 
-		 }
-		 return -1;
 	}
 }
