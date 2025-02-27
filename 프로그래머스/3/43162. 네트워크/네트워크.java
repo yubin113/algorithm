be73193517e7ee99bf53255fbaffef04d430de79
yubin113 <54315516+yubin113@users.noreply.github.com>
2025-02-27 13:21:18 +0900

    [level 3] Title: 네트워크, Time: 0.32 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,28 @@
+
+class Solution {
+    static boolean[] visited;
+
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        
+        visited = new boolean[computers.length];
+
+        for (int i = 0; i < computers.length; i++) {
+            if(!visited[i]){
+                answer++;
+                dfs(i, computers);
+            }
+        }
+        
+        return answer;
+    }
+        static void dfs(int node, int[][] computers){
+        visited[node] = true;
+
+        for (int i = 0; i < computers.length; i++) {
+            if(!visited[i] && computers[node][i] == 1){
+                dfs(i,  computers);
+            }
+        }
+    }
+}
