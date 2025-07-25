5aa524d1ea1a42cf87ce7c53224d52dde1df1fca
yubin113 <54315516+yubin113@users.noreply.github.com>
2025-07-25 14:11:30 +0900

    [level 2] Title: 예상 대진표, Time: 0.28 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+        while (a != b) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            answer++;
+        }
+
+        return answer;
+    }
+}
