14da865c14c943e7d3db8d0efa8ae1eb8db83df9
yubin113 <54315516+yubin113@users.noreply.github.com>
2025-07-28 09:07:47 +0900

    [level 2] Title: 타겟 넘버, Time: 4.45 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,25 @@
+class Solution {
+    int count = 0;
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        dfs(numbers, 0, target, 0);
+        answer = count;
+        return answer;
+    }
+    
+    public void dfs(int[] numbers, int depth, int target, int result){
+        //기저조건
+        if(depth == numbers.length){
+            if(target == result) count++;
+            return;
+        }
+        
+        int plus = result + numbers[depth];
+        int minus = result - numbers[depth];
+        
+        dfs(numbers, depth+1, target, plus);
+        dfs(numbers, depth+1, target, minus);
+        
+        
+    }
+}
